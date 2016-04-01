OCA/website

[ADD] website_event_share

website_event_share/__openerp__.py
@@ -1,0 +1,19 @@
+# -*- coding: utf-8 -*-
+# © 2016 Andhitia Rama <andhitia.r@gmail.com>
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+{
+    "name": "Event Share",
+    "version": "8.0.1.0.0",
+    "author": "Andhitia Rama,Odoo Community Association (OCA)",
+    "website": "https://opensynergy-indonesia.com",
+    "license": "AGPL-3",
+    "category": "Website",
+    "depends": [
+        "website_event",
+    ],
+    "data": [
+        "views/website_event.xml",
+    ],
+    "active": False,
+    'installable': True
+}

andhit-r/event

website_event_share/__openerp__.py
@@ -1,0 +1,19 @@
+# -*- coding: utf-8 -*-
+# © 2016 OpenSynergy Indonesia
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+{
+    "name": "Event Share",
+    "version": "8.0.1.0.0",
+    "author": "OpenSynergy Indonesia,Odoo Community Association (OCA)",
+    "website": "https://opensynergy-indonesia.com",
+    "license": "AGPL-3",
+    "category": "Website",
+    "depends": [
+        "website_event",
+    ],
+    "data": [
+        "views/website_event.xml",
+    ],
+    "active": False,
+    'installable': True
+}